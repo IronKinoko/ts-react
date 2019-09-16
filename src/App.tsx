151643e9fca688dc04420e316bdea4e0dac77f83
IronKinoko <kinoko_main@outlook.com>
2019-09-16 21:45:43 +0800

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
+import './App.sass'
 import React from 'react'
-import {hot} from 'react-hot-loader/root'
-import {Link} from 'react-router-dom'
-
-
+import { hot } from 'react-hot-loader/root'
 const App: React.FC = (props: { children?: React.ReactNode }) => {
-    return (
-        <div className="App">
-            hello world
-            <Link to={'/'}>home</Link>
-            <Link to={'/about'}>about</Link>
-            {props.children}
-        </div>
-    )
+  return <div className="App"></div>
 }
 
 export default process.env.NODE_ENV === 'development' ? hot(App) : App
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,19 @@
+import './index.sass'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import About from './components/About'
-import Home from './components/Home'
-import Child from './components/Child'
+import 'typeface-roboto'
 
 ReactDOM.render(
-    <Router>
-        <App>
-            <Route path="/" exact component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/about/child" component={Child}/>
-        </App>
-    </Router>
-    , document.getElementById('root'))
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
