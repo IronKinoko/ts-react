a754317c6495ab4b5cd79361d2f6e3b42cefd7a3
IronKinoko <kinoko_main@outlook.com>
2020-02-26 17:07:48 +0800

    feat: support IE9

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import './index.sass'
 import 'typeface-roboto'
+import 'core-js/es' 
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
+ 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
